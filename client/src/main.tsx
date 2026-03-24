30dfbc7aabfff3c121491b8daa198618c13f8673
guiaoyagui <guilhermeaoyaguiide@gmail.com>
2026-03-24 16:14:46 -0300

    fix: importar React explicitamente no main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,14 @@
-import { createRoot } from "react-dom/client";
+import React from "react"; // <--- ESTA LINHA É O SEGREDO
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Selecionamos o elemento 'root' do teu index.html
 const rootElement = document.getElementById("root");
 
-if (!rootElement) {
-  throw new Error("Não foi possível encontrar o elemento root no HTML.");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-// Renderização limpa: sem tRPC, sem Providers desnecessários
-createRoot(rootElement).render(
-  <App />
-);
